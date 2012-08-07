7a85e08e8455cc7d524286ebb2614ef97c338571
Val Neekman <val@neekware.com>
2012-08-06 20:10:49 -0700

    pull changes for split of Unique and Unicode into separate routines as well as ORM optimizations

--- pushpi.sh
+++ pushpi.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 bin/buildout setup . sdist register upload
 
+
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='django-uuslug',
-    version='0.7',
+    version='0.8',
     description = "A Django slugify application that guarantees uniqueness and handles unicode",
     long_description = read('README'),
     author='Val Neekman',
